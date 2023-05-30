676962f75e18cd4c52e93da8ace6c8e36591220d
Eric-Martin <gaspar.ericmartin@gmail.com>
2023-05-29 21:13:25 -0400

    Complete fetch on demand with forms code along

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form')
+    inputForm.addEventListener('submit', function (e) {
+        e.preventDefault()
+        const searchID = e.target.searchByID.value
+        
+        findAndDisplayMovieInfo(searchID)
+        
+    })
+    
+    function findAndDisplayMovieInfo(ID) {
+        fetch(`http://localhost:3000/movies/${ID}`)
+            .then((response) => response.json())
+            .then((data) => {
+                const title = document.querySelector('section#movieDetails h4')
+                const summary = document.querySelector('section#movieDetails p')
+                
+                title.textContent = data.title
+                summary.textContent = data.summary
+            
+                })
+            
+    }
 }
 
 document.addEventListener('DOMContentLoaded', init);
